178fa6834274c1afcef9b496a26cb11bfe10edcd
Dag Frode Solberg <dag.solberg@bekk.no>
2020-02-20 09:29:53 +0100

    Add classy-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import {Hello} from 'cdoc';
+import React from "react";
+import { c } from "classy-ui/macro";
 
 function App() {
-  return (
-
-          <Hello text="it works"/>
-     
-  );
+  return <h1 className={c("background-color-gray-400")}>Hello world</h1>;
 }
 
 export default App;
